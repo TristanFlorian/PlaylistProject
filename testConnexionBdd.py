f88015b11b38c12dcad7aece03d5246736586794
Florian THIERRY <florian.thierry72@gmail.com>
2014-10-08 14:56:33 +0200

    Le fichier test permet d'afficher tous les morceaux dont le titre est ''Toxic''

--- testConnexionBdd.py
+++ testConnexionBdd.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import motDePasse
 import sqlalchemy
-from sqlalchemy.sql.schema import Column
 
 # Déclaration et connexion au moteur sql utilisé
-engine = sqlalchemy.create_engine('posgresql://f.thierry:' + motDePasse.getMdp() + '@172.16.99.2:5432/radio_libre')
+engine = sqlalchemy.create_engine('postgresql://f.thierry:' + motDePasse.getMdp() + '@172.16.99.2:5432/radio_libre')
+# Connexion à la base
+conn = engine.connect()
 # Déclaration et initialisation des metadata
 metadata = sqlalchemy.MetaData()
-# Création de la structure de la bdd
-morceaux = sqlalchemy.Table('morceaux', metadata, Column('titre', str, primary_key = True),Column('album', str), Column('artiste', str), Column('genre', str), Column('sousgenre', str), Column('duree',int(7)), Column('format',str), Column('polyphonie',int(9)),Column('chemin',str))
-# Création de la base dans le modèle python
-morceaux.create_all(engine)
+# Création de la sqlalchemy.Stringucture de la bdd
+morceaux = sqlalchemy.Table('morceaux', metadata, 
+                            sqlalchemy.Column('titre', sqlalchemy.String, primary_key = True), 
+                            sqlalchemy.Column('album', sqlalchemy.String), sqlalchemy.Column('artiste', sqlalchemy.String), 
+                            sqlalchemy.Column('genre', sqlalchemy.String), sqlalchemy.Column('sousgenre', sqlalchemy.String), 
+                            sqlalchemy.Column('duree', sqlalchemy.Integer), sqlalchemy.Column('format',sqlalchemy.String), 
+                            sqlalchemy.Column('polyphonie',sqlalchemy.Integer), sqlalchemy.Column('chemin',sqlalchemy.String))
+# Affichage du contenu de la table morceaux
+s = sqlalchemy.select([morceaux]).where(morceaux.c.titre == 'Toxic')
+result = conn.execute(s)
 
+for row in result:
+    print(row)
